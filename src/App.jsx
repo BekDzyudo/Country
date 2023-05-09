01d540acdee39ee96ce194a304f59c28876d7a56
BekDzyudo <jalilovshohbek9909@gmail.com>
2023-05-09 12:47:35 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,19 @@ import Card from "./components/card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
 import Form from "./components/form";
+import { Loader } from "./components/loader";
 
 function App() {
   const [data, setData] = useState([]);
+  const [loader, setLoader] = useState(true);
 
   function Country() {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
-      .then((data) => setData(data))
+      .then((data) => {
+        setData(data);
+        setLoader(false);
+      })
       .catch((err) => console.log(err));
   }
 
@@ -20,26 +25,29 @@ function App() {
 
   return (
     <>
-      <div>
-        <Header />
-        <div className="container">
-          <Form data={data} setData={setData} />
-          <ul className="list-unstyled d-flex gap-5 flex-wrap mt-4">
-            {data.map((item) => {
-              return (
-                <Card
-                  key={item.name.common}
-                  flag={item.flags.svg}
-                  name={item.name.common}
-                  region={item.region}
-                  population={item.population}
-                  capital={item.capital}
-                />
-              );
-            })}
-          </ul>
+      {loader && <Loader />}
+      {!loader && (
+        <div>
+          <Header />
+          <div className="container">
+            <Form data={data} setData={setData} />
+            <ul className="list-unstyled d-flex gap-5 flex-wrap mt-4">
+              {data.map((item) => {
+                return (
+                  <Card
+                    key={item.name.common}
+                    flag={item.flags.svg}
+                    name={item.name.common}
+                    region={item.region}
+                    population={item.population}
+                    capital={item.capital}
+                  />
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
---
+++ src/components/loader.jsx
@@ -0,0 +1,19 @@
+import "./loader.css";
+export const Loader = () => {
+  return (
+    <div className="loadingio-spinner-bean-eater-3jt4xpzzq8b">
+      <div class="ldio-391ig72pwlj">
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
